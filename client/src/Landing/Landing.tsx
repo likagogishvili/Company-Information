1923aa60d9e19032d4a4af788fe0d6eb4c43a8ad
likagogishvili <likagogishvili2@gmail.com>
2023-04-19 23:33:04 +0400

    added google authentication

--- client/src/Landing/Landing.tsx
+++ client/src/Landing/Landing.tsx
@@ -1,35 +1,55 @@
 import "./landing.scss";
 import * as img from "../assets/LangingPage/langingImg";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
+declare global {
+  interface Window {
+    google: any;
+  }
+}
 function Landing() {
+  const [user, setUser] = useState<any>({});
   function handleCallbackResponse(response: any) {
-    console.log("Encoded JWT ID token ", response.credential);
-    // var userObject = 
+    let userObject = jwtDecode(response.credential);
+    document.getElementById("signInBTN")!.hidden = true;
+    if (userObject) setUser(userObject);
+  }
+  function handleSignOut() {
+    setUser({});
+    document.getElementById("signInBTN")!.hidden = false;
   }
 
   useEffect(() => {
-    /* global google */
-    //@ts-ignore
-    google.accounts.id.initialize({
-      client_id:
-        "999430817633-hvb6en56r7u46belo07j8fl880rla1ag.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-    //@ts-ignore
-    google.accounts.id.renderButton(document.getElementById("signInBTN"), {
-      theme: "outline",
-      size: "large",
-    });
+    if (window.google) {
+      window.google.accounts.id.initialize({
+        client_id:
+          "999430817633-hvb6en56r7u46belo07j8fl880rla1ag.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+      });
+      window.google.accounts.id.renderButton(
+        document.getElementById("signInBTN"),
+        {
+          theme: "outline",
+          size: "large",
+        }
+      );
+    }
   }, []);
+
   return (
     <div className="bgImg" style={{ backgroundImage: `url(${img.landingBg})` }}>
-      <button
-        id="signInBTN"
-        className="rounded shawdow p-2 bg-white text-yellow-400"
-      >
-        Hello world!
-      </button>
+      {Object.keys(user).length ? (
+        <div>
+          <img src={user.picture} alt="" width="20px" height="20px" />
+          <h3>{user.name}</h3>
+          <button id="signOut" onClick={handleSignOut}>
+            Sign out
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+      <button id="signInBTN">Sign In</button>
     </div>
   );
 }
