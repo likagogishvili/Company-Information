77dc22f567b5ea5ada48544e57e7ab67c14b7e72
ლიკა გოგიშვილი <lgogishvili@geostat.gov.ge>
2023-04-28 17:14:14 +0400

    added animations

---
+++ client/src/Company/Company.tsx
@@ -0,0 +1,25 @@
+import "./company.scss";
+function Company() {
+  return (
+    <div className="company">
+      <img
+        width="400px"
+        src="https://img.freepik.com/free-photo/business-people-walking-outside-office-building_107420-74359.jpg?w=1380&t=st=1682682773~exp=1682683373~hmac=7ed7e6555c760b11f41ce8fe1e70bd435db231f2836bd74a2c046bcd53526454"
+      />
+      <div className="box">
+        <p>
+          Collaboratively administrate empowered markets via plug-and-play
+          networks.
+        </p>
+        <p>
+          Dynamically procrastinate B2C users after installed base benefits.
+        </p>
+        <p>
+          Dramatically visualize customer directed convergence without
+          revolutionary ROI.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Company;
--- client/src/Landing/Landing.tsx
+++ client/src/Landing/Landing.tsx
@@ -2,16 +2,18 @@ import "./landing.scss";
 import * as img from "../assets/LangingPage/langingImg";
 import SignIn from "../SignIn/SignIn";
 import { useShowSignIn } from "../store/selectors/Selctors";
+import Company from "../Company/Company";
 function Landing() {
   return (
     <div
-      className="bgImg"
+      className="bgImg "
       style={{ backgroundImage: `url(${img.landingBg})` }}
     >
-      {
-        useShowSignIn() && <SignIn/>
-      }
-      
+      <div className="welcomediv">
+        <h1>Welcome To Our Page</h1>
+      </div>
+      {useShowSignIn() && <SignIn />}
+      <Company />
     </div>
   );
 }
